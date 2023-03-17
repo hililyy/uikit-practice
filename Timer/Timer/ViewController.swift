9b74650bd9224010efe2fdad90208185f584e0ba
hililyy <joun406@gmail.com>
2023-03-17 17:18:50 +0900

    [FEAT] 설정한 시간에 따른 타이머 추가

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    weak var timer: Timer?
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
-
+    
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
+        slider.value = 0.5
+    }
+    
+    @objc func doSomethingAfter1Second() {
+        if number > 0 {
+            number -= 1
+            slider.value = Float(number) / Float(60)
+            mainLabel.text = "\(number) 초"
+        } else {
+            number = 0
+            mainLabel.text = "초를 선택하세요"
+            timer?.invalidate()
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let seconds = Int(sender.value * 60)
+        mainLabel.text = "\(seconds) 초"
+        number = seconds
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomethingAfter1Second), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        mainLabel.text = "초를 선택하세요"
+        slider.value = 0.5
+        number = 0
+        timer?.invalidate()
+    }
 }
-
