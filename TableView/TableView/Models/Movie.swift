353b0a743080a26e0390e9cb1b146df27fc90dae
hililyy <joun406@gmail.com>
2023-05-29 15:58:20 +0900

    [FEAT] 테이블뷰 데이터 셋팅

---
+++ TableView/TableView/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by 강조은 on 2023/05/29.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
