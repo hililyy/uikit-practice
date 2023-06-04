782aec052dcc84d57426a5dba35ea08a7712d164
hililyy <joun406@gmail.com>
2023-06-04 20:31:16 +0900

    [FEAT[ 제스처, 피커뷰 생성

--- MemberList/MemberList/Views/DetailView.swift
+++ MemberList/MemberList/Views/DetailView.swift
@@ -226,6 +226,7 @@ class DetailView: UIView {
         super.init(frame: frame)
         backgroundColor = .white
         setupStackView()
+        setupMemberIdTextField()
     }
     
     required init?(coder: NSCoder) {
@@ -234,6 +235,10 @@ class DetailView: UIView {
     
     func setupStackView() {
         self.addSubview(stackView)
+    }
+    
+    func setupMemberIdTextField() {
+        memberIdTextField.delegate = self
     }
     
     // 오토레이아웃 업데이트
@@ -272,5 +277,18 @@ class DetailView: UIView {
             stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
         ])
+    }
+}
+
+extension DetailView: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 멤버 아이디는 수정 못하도록 설정 (멤버아이디의 텍스트필드는 입력 안되도록 설정)
+        if textField == memberIdTextField {
+            return false
+        }
+        
+        // 나머지 텍스트필드는 관계없이 설정 가능
+        return true
     }
 }
